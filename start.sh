68dfe6f62c1ac0fd8df1cb2fe76486511edfdb5c
Neerj Kumar <119673950+golubhaii@users.noreply.github.com>
2023-10-12 06:24:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://github.com/golubhaii/KaLa-ToTka/tree/master /KaLa-ToTka
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /KaLa-ToTka
 fi
-cd /Advance-Auto-Filter
+cd /KaLa-ToTka
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
